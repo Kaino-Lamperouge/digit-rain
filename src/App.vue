c5976a4e88dede99d3e6906cc4212a5668b3b12f
zmt <1832612650@qq.com>
2023-06-28 10:34:05 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <!-- <VueMatrixRaindrop></VueMatrixRaindrop> -->
-    <VueMatrix></VueMatrix>
+    <VueMatrixRaindrop></VueMatrixRaindrop>
+    <!-- <VueMatrix></VueMatrix> -->
   </div>
 </template>
 
 <script>
 
-// import VueMatrixRaindrop from './view/vue-matrix-raindrop'
-import VueMatrix from './view/vue-matrix-017'
+import VueMatrixRaindrop from './view/vue-matrix-raindrop'
+// import VueMatrix from './view/vue-matrix-017'
 export default {
   name: 'App',
   components:{
-    // VueMatrixRaindrop,
-    VueMatrix
+    VueMatrixRaindrop,
+    // VueMatrix
   }
 }
 </script>
